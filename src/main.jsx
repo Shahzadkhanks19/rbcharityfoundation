d2261e9c1b61686e7f79ad526ce6433d0ac5e41b
Shahzad Khan <joykhan78600@gmail.com>
2026-08-20 22:22:33 +0530

    Add global preloader and error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { AppPreloader, GlobalErrorBoundary } from './components/system/SystemUI'
 import PlatformRouter from './router/PlatformRouter'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PlatformRouter />
+    <GlobalErrorBoundary>
+      <AppPreloader>
+        <PlatformRouter />
+      </AppPreloader>
+    </GlobalErrorBoundary>
   </React.StrictMode>
 )
