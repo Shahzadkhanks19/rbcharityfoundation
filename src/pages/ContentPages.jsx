9a42952c1741623ab6b8145db09d1f0f26cdb315
Shahzad Khan <joykhan78600@gmail.com>
2026-08-21 20:20:29 +0530

    Use renamed transparency content module

--- src/pages/ContentPages.jsx
+++ src/pages/ContentPages.jsx
@@ -1,5 +1,5 @@
 import { ShieldCheck } from 'lucide-react'
-import { transparencySteps } from '../data/contentSeed'
+import { transparencySteps } from '../data/transparencyContent'
 
 const shell='mx-auto w-full max-w-7xl px-5 sm:px-6 lg:px-8'
 
